70e7632b206a632761f1d2a448c1e3cefc5d5cdb
Eduardo Alves <eduardoa@prognus.com.br>
2014-05-22 14:57:46 -0300

    Fixes #90 - Correção na exibição de múltiplos alertas de lembrete

--- prototype/modules/calendar/js/calendar.alarms.js
+++ prototype/modules/calendar/js/calendar.alarms.js
@@ -3,14 +3,13 @@ Alarms = {
 	var eventsDay = DataLayer.get('alarm:schedulable',['=', 'date', Date.today().getTime()]);
 	if(eventsDay)
 	    for(var i = 0; i < eventsDay.length; i++){
-		this.addAlarm( eventsDay[i] );
+		    this.addAlarm( eventsDay[i] );
 	    }
     },
 
-    addAlarm: function( eventDay ){            
+    addAlarm: function( eventDay ){
 	if(!DataLayer.tasks[parseInt(eventDay.sendTime)]){
 	    DataLayer.task( parseInt(eventDay.sendTime) , function( timestamp ){
-
 		if(!activePage && useDesktopNotification() && desktopNotification.verifyComplement()){
 
 		    desktopNotification.sentNotification(
@@ -31,22 +30,28 @@ Alarms = {
 			});
 
 		}else{
-
 		    var path = User.moduleName == 'expressoCalendar' ? '' : '../prototype/modules/calendar/';
 		    DataLayer.render(path+'templates/alarm.ejs',{
 			event: eventDay
-		    }, function( html ){                                
-			$.Zebra_Dialog(html , {
-			    'type':     'question',
-			    'overlay_opacity': '0.5',
-			    'buttons':  ['_[[Close]]'],
-			    'onClose':  function(clicked) {}
-			});
-		    });
-		}
-
-	    });
-	}
+                },function( html ){
+                        if($('.ZebraDialog').length){
+                            var cssTop = parseInt($('.ZebraDialog').css('top'));
+                            if(!$('.ZebraDialog_Body').find('#event_' + eventDay.id).length){
+                                $('.ZebraDialog_Body div:eq(0)').append('<hr/>' + html);
+                                $('.ZebraDialog').css('top', cssTop-60);
+                            }
+                        }else{
+                            $.Zebra_Dialog(html , {
+                                'type':     'information',
+                                'overlay_opacity': '0.5',
+                                'buttons':  ['_[[Close]]'],
+                                'onClose':  function(clicked) {}
+                            });
+                        }
+                    });
+                }
+	        });
+	    }
     }
 }
 Alarms.load();
