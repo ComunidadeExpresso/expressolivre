48c2355a7c8dbe4b3d29e31cd6a804b7a502621a
Eduardo Alves <eduardoa@prognus.com.br>
2014-05-22 16:15:45 -0300

    Fixes #88 - Reversão de arrays utilizados em outras chamadas do dateCalendar

--- prototype/modules/calendar/js/calendar.date.js
+++ prototype/modules/calendar/js/calendar.date.js
@@ -1,5 +1,69 @@
 dateCalendar = {
 
+    monthNamesShort:
+        [
+            '_[[Jan]]',
+            '_[[Feb]]',
+            '_[[Mar]]',
+            '_[[Apr]]',
+            '_[[May]]',
+            '_[[June]]',
+            '_[[July]]',
+            '_[[Aug]]',
+            '_[[Sept]]',
+            '_[[Oct]]',
+            '_[[Nov]]',
+            '_[[Dec]]'
+        ],
+
+    //MonthOfNumber
+    monthNames:
+        [
+            '_[[January]]',
+            '_[[February]]',
+            '_[[March]]',
+            '_[[April]]',
+            '_[[May]]',
+            '_[[June]]',
+            '_[[July]]',
+            '_[[August]]',
+            '_[[September]]',
+            '_[[October]]',
+            '_[[November]]',
+            '_[[December]]'
+        ],
+    //weekOfDay
+    dayNames:
+        [
+            '_[[Sunday]]',
+            '_[[Monday]]',
+            '_[[Tuesday]]',
+            '_[[Wednesday]]',
+            '_[[Thursday]]',
+            '_[[Friday]]',
+            '_[[Saturday]]'
+        ],
+    dayNamesShort:
+        [
+            '_[[Sun]]',
+            '_[[Mon]]',
+            '_[[Tue]]',
+            '_[[Wed]]',
+            '_[[Thu]]',
+            '_[[Fri]]',
+            '_[[Sat]]'
+        ],
+    dayNamesShortest:
+        [
+            '_[[SundayShort]]',
+            '_[[MondayShort]]',
+            '_[[TuesdayShort]]',
+            '_[[WednesdayShort]]',
+            '_[[ThursdayShort]]',
+            '_[[FridayShort]]',
+            '_[[SaturdayShort]]'
+        ],
+
     dayOfWeek:
     {
         'SUN': 0,
